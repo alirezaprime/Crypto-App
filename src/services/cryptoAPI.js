7b30858c88bd490a6889ecd9b766625034d4f857
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2024-07-30 15:07:25 +0330

    ta ghabl az ezafe kardane modal

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,7 +1,9 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-GxtEHPJSLkAFWbbbEtFrdedw";
 
-const getCoinList = (page, currency) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&ids=bitcoin&category=layer-1&order=market_cap_desc&per_page=20&page=${page}&locale=en&x_cg_demo_api_key=${API_KEY}`;
-};
-export { getCoinList };
+const getCoinList = (page, currency) =>
+  `${BASE_URL}/coins/markets?vs_currency=${currency}&ids=bitcoin&category=layer-1&order=market_cap_desc&per_page=20&page=${page}&locale=en&x_cg_demo_api_key=${API_KEY}`;
+
+const searchCoin = (query) =>
+  `${BASE_URL}/search?query=${query}&locale=en&x_cg_demo_api_key=${API_KEY}`;
+export { getCoinList, searchCoin };
